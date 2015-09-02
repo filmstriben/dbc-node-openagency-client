0839425bdab9650dff08eac6b39a6a55c7521f93
Jonas Peter Hyatt <jph@dbc.dk>
2015-09-02 13:00:00 +0200

    Fix client bug relating to persistance of queries, due to a variable in a higher scope...

--- src/__tests__/openagency.test.js
+++ src/__tests__/openagency.test.js
@@ -3,7 +3,30 @@
 import * as OpenAgency from '../client.js';
 
 describe('Test OpenAgency Client', () => {
-  it('Asserts OpenAgency can be queried with an agency ID', function(done) {
+  it('Dummy OpenAgency test for querying search', function(done) {
+    const timeout = 10000;
+    this.timeout(timeout);
+    setTimeout(done, timeout);
+
+    const config = {
+      wsdl: 'http://openagency.addi.dk/2.22/openagency.wsdl'
+    };
+
+    OpenAgency.init(config);
+    let result = OpenAgency.searchOpenAgency({
+      query: 'valb'
+    });
+
+    done();
+
+    /* eslint-disable no-unused-vars */
+    result.then(function(searchResult) {
+      /* eslint-enable no-unused-vars */
+      // Mostly used for testing during dev, not actually a unit test
+    });
+  });
+
+  it('Dummy OpenAgency test for querying agency ID', function(done) {
     const timeout = 10000;
     this.timeout(timeout);
     setTimeout(done, timeout);
@@ -14,30 +37,7 @@ describe('Test OpenAgency Client', () => {
 
     OpenAgency.init(config);
     let result = OpenAgency.getOpenAgency({
-      id: '715100'
-    });
-
-    done();
-
-    /* eslint-disable no-unused-vars */
-    result.then(function(searchResult) {
-    /* eslint-enable no-unused-vars */
-      // Mostly used for testing during dev, not actually a unit test
-    });
-  });
-
-  it('Asserts OpenAgency can be queried with a search request', function(done) {
-    const timeout = 10000;
-    this.timeout(timeout);
-    setTimeout(done, timeout);
-
-    const config = {
-      wsdl: 'http://openagency.addi.dk/2.22/?wsdl/openagency.wsdl'
-    };
-
-    OpenAgency.init(config);
-    let result = OpenAgency.searchOpenAgency({
-      query: 'valb'
+      id: '710120'
     });
 
     done();
